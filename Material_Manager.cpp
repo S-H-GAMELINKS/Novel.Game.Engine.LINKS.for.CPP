60349d2fff38555ce73dff76909beb049db94aa5
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-22 22:34:21 +0900

    using SerCreateSoundDataType for Memory

--- Material_Manager.cpp
+++ Material_Manager.cpp
@@ -120,6 +120,9 @@ namespace {
 //各種素材読込関数
 void MaterialLoad(std::array<int, 99>& BackGround, std::array<int, 99>& Character, std::array<int, 99>& BackGroundMusic, std::array<int, 99>& SoundEffect, std::array<std::string, 99>& Movie) {
 
+	//サウンドデータの読み込み形式
+	DxLib::SetCreateSoundDataType(DX_SOUNDDATATYPE_MEMPRESS);
+
 	//背景画像読込関数
 	MaterialLoadBackGround(BackGround);
 
