5f1c999d8efe03d9a081fc8081bd62e3be9ecdbe
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-22 17:42:30 +0900

    add DxLibPreProccessing & DxLibPostProccesing Func

--- main.cpp
+++ main.cpp
@@ -9,9 +9,17 @@
 #include <string>
 #include <fstream>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nCmdShow)
-{
+//DxLib初期化前処理
+void DxLibInitPreProccessing() {
+	//ログの抽出
+	DxLib::SetOutApplicationLogValidFlag(FALSE);
+
+	//アイコンの指定
+	DxLib::SetWindowIconID(101);
+
+	//ウインドウのサイズを任意に変更可能
+	DxLib::SetWindowSizeChangeEnableFlag(TRUE, TRUE);
+
 	//ウインドウサイズ指定
 	DxLib::SetGraphMode(640, 480, 16);
 
@@ -21,18 +29,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	//ゲームタイトルの指定
 	DxLib::SetMainWindowText(GameTitle);
 
-	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
-	{
-		return -1;				// エラーが起きたら直ちに終了
-	}
+	//ウインドウスタイルの指定
+	DxLib::SetWindowStyleMode(7);
+}
 
+//DxLib初期化後処理
+void DxLibInitPostProccessing() {
 	// フォントのサイズセット
 	SetFontSize(moji_size);
 
 	//スクリプト読込関数
 	ScriptRead(Script, EndFlag);
 
+	//各種素材の読込関数
 	MaterialLoad(BackGround, Character, BackGroundMusic, SoundEffect, Movie);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	//初期化前処理
+	DxLibInitPreProccessing();
+
+	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
+	{
+		return -1;				// エラーが起きたら直ちに終了
+	}
+
+	//初期化後処理
+	DxLibInitPostProccessing();
 
 	// ループ
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
