4df920536a1172ff56add2ebb14db7c3f7f111f7
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-26 16:44:05 +0900

    Add BackGroundMusicVolChange Func

--- ConfigMenu.cpp
+++ ConfigMenu.cpp
@@ -3,9 +3,23 @@
 #include "DxLib.h"
 #include "ConstantExpressionVariable.h"
 #include "Utility.h"
+#include "ConfigMenu.h"
 #include <thread>
 #include <chrono>
 
+ConfigData_t ConfigData = {
+	/*bgm_vol               :*/100,
+	/*bgm_vol_count         :*/10,
+	/*se_vol                :*/100,
+	/*se_vol_count          :*/10,
+	/*skip_speed            :*/100,
+	/*skip_speed_count      :*/10,
+	/*auto_speed            :*/100,
+	/*auto_speed_count      :*/10,
+	/*string_speed          :*/100,
+	/*string_speed_count    :*/10,
+	/*mouse_key_move        :*/1
+};
 
 namespace {
 
@@ -21,6 +35,8 @@ namespace {
 			DxLib::DrawString(save_name_pos_x, game_menu_base_pos_y * (i + 1), ConfigMenuItem[i], 255);
 
 		DxLib::DrawString(save_name_pos_x - cursor_move, cursor_y, "■", 255);
+
+		DxLib::DrawFormatString(save_name_pos_x + cursor_move * 5, game_menu_base_pos_y, 255, "%d", ConfigData.bgm_vol);
 	}
 
 	//コンフィグ画面キー操作
@@ -34,8 +50,38 @@ namespace {
 		std::this_thread::sleep_for(std::chrono::milliseconds(wait_key_task_time));
 	}
 
+	//BGM音量調節
+	void BackGroundMusicVolChange() {
+		if (CheckHitKey(KEY_INPUT_RIGHT) == 1) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(wait_key_task_time));
+
+			ConfigData.bgm_vol += 10;
+			ConfigData.bgm_vol_count += 1;
+
+			if (ConfigData.bgm_vol_count >= 10) {
+				ConfigData.bgm_vol = 100;
+				ConfigData.bgm_vol_count = 10;
+			}
+		}
+
+		if (CheckHitKey(KEY_INPUT_LEFT) == 1) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(wait_key_task_time));
+
+			ConfigData.bgm_vol -= 10;
+			ConfigData.bgm_vol_count -= 1;
+
+			if (ConfigData.bgm_vol_count <= 0) {
+				ConfigData.bgm_vol = 0;
+				ConfigData.bgm_vol_count = 0;
+			}
+		}
+	}
+
 	//コンフィグ画面選択処理
 	void ConfigMenuSelect(std::int32_t& cursor_y, std::int32_t& ConfigFlag) {
+
+		if (game_menu_base_pos_y == cursor_y)
+			BackGroundMusicVolChange();
 
 		if (game_menu_base_pos_y * 8 == cursor_y && DxLib::CheckHitKey(KEY_INPUT_RETURN) == 1) {
 			if (IDYES == MessageBoxYesNo("戻りますか？")) {
--- ConfigMenu.h
+++ ConfigMenu.h
@@ -1,7 +1,25 @@
 #ifndef CONFIG_MENU_H_
 #define CONFIG_MENU_H_
 
+#include <cstdint>
+
 //コンフィグ画面ループ
 void ConfigMenuLoop();
+
+//設定データ
+struct alignas(4) ConfigData_t {
+	std::int32_t bgm_vol;				//BGM音量情報
+	std::int32_t bgm_vol_count;			//BGM音量メーター情報
+	std::int32_t se_vol;					//SE音量情報
+	std::int32_t se_vol_count;			//SE音量メーター情報
+	std::int32_t skip_speed;				//スキップ速度情報
+	std::int32_t skip_speed_count;		//スキップ速度メーター情報
+	std::int32_t auto_speed;				//オート速度情報
+	std::int32_t auto_speed_count;		//オート速度メーター情報
+	std::int32_t string_speed;			//文字列描画速度
+	std::int32_t string_speed_count;		//文字列描画速度メーター情報
+	std::int32_t mouse_key_move;			//マウス操作とキー操作の情報 true/false
+};
+
 
 #endif // !CONFIG_MENU_H_
