24fab6e22142926ecf25b436d756099f39c1c55c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-02-05 13:51:40 +0900

    fix indent

--- ConstantExpressionVariable.h
+++ ConstantExpressionVariable.h
@@ -8,19 +8,19 @@ constexpr int MaterialMax = 99;
 constexpr int MojiSize = 21;
 
 //タイトルメニュー位置
-constexpr int TitleMenuPosY = 300;
-constexpr int TitleMenuPosX = 430;
-constexpr int TitleMenuLoadPosY = 330;
-constexpr int TitleMenuConfigPosY = 360;
+constexpr int TitleMenuPosY			 = 300;
+constexpr int TitleMenuPosX			 = 430;
+constexpr int TitleMenuLoadPosY		 = 330;
+constexpr int TitleMenuConfigPosY	 = 360;
 constexpr int TitleMenuQuickLoadPosY = 390;
-constexpr int TitleMenuContinuePosY = 420;
-constexpr int TitleMenuExitPosY = 450;
+constexpr int TitleMenuContinuePosY  = 420;
+constexpr int TitleMenuExitPosY		 = 450;
 
 //タイトルメニューのカーソル位置
 constexpr int TitleMenuCursorPosX = 400;
 
 //選択肢
-constexpr int ChoicePosX = 100;
+constexpr int ChoicePosX   = 100;
 constexpr int ChoicePosY[] = { 200, 230 };
 
 //キーカーソルの移動量
@@ -36,13 +36,13 @@ constexpr int WaitGameTime = 3;
 constexpr int GameMenuBasePosY = 30;
 
 //セーブ・ロード画面関係
-constexpr int SaveDataSlotNum = 3;
-constexpr int SaveDataBasePosX = 400;
-constexpr int SaveDataBasePosY = 100;
+constexpr int SaveDataSlotNum    = 3;
+constexpr int SaveDataBasePosX   = 400;
+constexpr int SaveDataBasePosY   = 100;
 constexpr int SaveDataCursorMove = 100;
-constexpr int SaveDataPosButtom = SaveDataBasePosY + SaveDataCursorMove * SaveDataSlotNum;
-constexpr int SaveSnapDrawPosX = 150;
-constexpr int SaveDataNamePosX = 250;
+constexpr int SaveDataPosButtom  = SaveDataBasePosY + SaveDataCursorMove * SaveDataSlotNum;
+constexpr int SaveSnapDrawPosX   = 150;
+constexpr int SaveDataNamePosX   = 250;
 
 //文字描画速度関係
 constexpr int StringSpeedSkip = 10;
@@ -52,7 +52,7 @@ constexpr int StringSpeedWait = 600;
 //立ち絵位置関係
 constexpr int CharacterPosY = 130;
 constexpr int CharacterPosX = 150;
-constexpr int CharacterGraphSizeWidth = 300;
+constexpr int CharacterGraphSizeWidth  = 300;
 constexpr int CharacterGraphSizeHeight = 400;
 
 #endif // CONSTANT_EXPRESSION_VARIABLE_H_
