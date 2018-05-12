ddce2950958d73c835d52ec74c785fd7d0ef909c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-05-12 18:28:02 +0900

    fix comment

--- ScriptTask_Manager.cpp
+++ ScriptTask_Manager.cpp
@@ -149,7 +149,7 @@ namespace ScriptTask {
 			DxLib::StopSoundMem(Handle);
 	}
 
-	//BGMÄ¶ŠÖ”
+	//‰¹Œ¹Ä¶ŠÖ”
 	template <typename T>
 	void PlaySounds(Script& Script, Material<int>& Material, T& Handle, const T& PlayType) noexcept {
 
