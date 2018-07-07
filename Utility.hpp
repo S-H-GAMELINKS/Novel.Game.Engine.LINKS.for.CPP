dba3627fe1a2b1e3a0364a6fb3ffc50ec917534b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-07 21:01:05 +0900

    add Utility.hpp

---
+++ Utility.hpp
@@ -0,0 +1,28 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+//メッセージボックス(Yes or No)
+int MessageBoxYesNo(LPCTSTR lpText) noexcept;
+
+//メッセージボックス(OK)
+int MessageBoxOk(LPCTSTR lpText) noexcept;
+
+//画面クリア処理関数
+void ScreenClear() noexcept;
+
+//ゲーム終了確認ウインドウ
+void GameEndMessageBox() noexcept;
+
+//各種ショートカットキー
+void ShortCutKey() noexcept;
+
+//タイトルムービー処理
+void TitleMoviePlay() noexcept;
+
+//メニューバー
+void MenuBarDraw();
+
+//メニューバー選択処理
+void MenuBarSelect();
+
+#endif // !UTILITY_H_
