ae94ee6e7d947934d9f9bb4ea5d8d31fab3dc71e
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-09-02 21:51:13 +0900

    fix KeyState in BackLogKeyMove func

--- BackLog.cpp
+++ BackLog.cpp
@@ -13,6 +13,10 @@ namespace BackLog {
 	std::vector<std::int32_t> BackLog;
 	std::string Name = "バックログ";
 
+	//判定用ラムダ式
+	auto MouseCheck = [](const int& Args) {return (DxLib::CheckHitKey(Args) == 1); };
+	auto KeyCheck = [](const int& Args) {return (DxLib::GetMouseInput() == Args); };
+
 	//バックログのナンバー表示
 	void BackLogNumberDraw(const std::int32_t& Num) noexcept {
 		DxLib::DrawString(0, 400, (Name + std::to_string(Num + 1)).c_str(), Color);
@@ -37,10 +41,10 @@ namespace BackLog {
 	//バックログ時のキー操作
 	void BackLogKeyMove(std::int32_t& Num, bool& BackLogFlag) noexcept {
 
-		if (DxLib::CheckHitKey(KEY_INPUT_UP) == 1)
+		if (MouseCheck(KEY_INPUT_UP) || KeyCheck(MOUSE_INPUT_RIGHT))
 			Num = (Num < BackLogCount) ? Num + 1 : Num;
 
-		if (DxLib::CheckHitKey(KEY_INPUT_DOWN) == 1)
+		if (MouseCheck(KEY_INPUT_DOWN) || KeyCheck(MOUSE_INPUT_LEFT))
 			Num = (Num > 0) ? Num - 1 : 0;
 
 		if (DxLib::CheckHitKey(KEY_INPUT_BACK) == 1) {
